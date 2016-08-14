0be159d81923b5a5a1dd71c90507acec522593ad
azure820529 <azure820529@gmail.com>
2016-08-14 18:32:47 +0800

    add rc-calendar lib and css webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,33 @@
 var config = {
-   entry: './main.js',
+  entry: './main.js',
 
-   output: {
-      path:'./',
-      filename: 'index.js',
-   },
+  output: {
+    path:'./',
+    filename: 'index.js',
+  },
 
-   devServer: {
-      inline: true,
-      port: 8085
-   },
+  devServer: {
+    inline: true,
+    port: 8085
+  },
 
-   module: {
-      loaders: [
-         {
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
 
-            query: {
-               presets: ['react','es2015']
-            }
-         }
-      ]
-   }
+        query: {
+          presets: ['react','es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  }
 }
 
 module.exports = config;
