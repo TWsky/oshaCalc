0091f34db74334ba284c2ce1ecc18dacb39baac5
azure820529 <azure820529@gmail.com>
2016-08-16 16:57:40 +0800

    simplify calendar component

--- components/Calendar.js
+++ components/Calendar.js
@@ -4,10 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Picker from 'rc-calendar/lib/Picker';
 
-import 'rc-time-picker/assets/index.css';
-
 import zhCN from 'rc-calendar/lib/locale/zh_CN';
-import enUS from 'rc-calendar/lib/locale/en_US';
 import 'rc-time-picker/assets/index.css';
 import TimePickerPanel from 'rc-time-picker/lib/Panel';
 
@@ -16,120 +13,24 @@ import 'moment/locale/zh-cn';
 import 'moment/locale/en-gb';
 
 const formatStr = 'YYYY-MM-DD';
-const cn = location.search.indexOf('cn') !== -1;
 
 const now = moment();
-var selectedArr = [];
-if (cn) {
-  now.locale('zh-cn').utcOffset(8);
-} else {
-  now.locale('en-gb').utcOffset(0);
-}
-
-const defaultCalendarValue = now.clone();
-defaultCalendarValue.add(-1, 'month');
-
-const timePickerElement = <TimePickerPanel />;
 
 const divStyle = {
   display: 'inline-block',
   width: '40%',
   margin: '5%'
-
-
 };
-
-function disabledDate(current) {
-  const date = moment();
-  date.hour(0);
-  date.minute(0);
-  date.second(0);
-  return current.isBefore(date);  // can not select days before today
-}
 
 function format(v) {
   return v ? v.format(formatStr) : '';
 }
 
-function isValidRange(v) {
-  return v && v[0] && v[1];
-}
 
-function onStandaloneChange(value) {
-  console.log('onChange');
-  console.log(value[0] && format(value[0]), value[1] && format(value[1]));
-}
-
-
-
-// const Test = React.createClass({
-//   getInitialState() {
-//     return {
-//       value: [],
-//     };
-//   },
-//
-//   onChange(value) {
-//     this.setState({ value });
-//   },
-//
-//   render() {
-//     const state = this.state;
-//     const calendar = (
-//       <RangeCalendar
-//         showWeekNumber={false}
-//         dateInputPlaceholder={['start', 'end']}
-//         defaultValue={[now, now]}
-//         locale={cn ? zhCN : enUS}
-//         disabledDate={disabledDate}
-//         timePicker={timePickerElement}
-//       />
-//     );
-//     return (<Picker
-//       value={state.value}
-//       onChange={this.onChange}
-//       animation="slide-up"
-//       calendar={calendar}
-//     >
-//       {
-//         ({ value }) => {
-//           return (<span>
-//                 <input
-//                   placeholder="please select"
-//                   style={{ width: 350 }}
-//                   disabled={state.disabled}
-//                   readOnly
-//                   className="ant-calendar-picker-input ant-input"
-//                   value={isValidRange(value) && `${format(value[0])} - ${format(value[1])}` || ''}
-//                 />
-//                 </span>);
-//         }
-//       }
-//     </Picker>);
-//   },
-// });
-export class ResultCalendar extends React.Component {
+class ResultCalendar extends React.Component {
   constructor(props) {
-    super(props);
-    this.onStandaloneSelect = this.onStandaloneSelect.bind(this);
-
-    this.state = {
-      selectedArr: [],
-      diffDate: 0
-    };
-  }
-
-  onStandaloneSelect(value) {
-    var selectedArr = [];
-    var diffDate = 0;
-    // console.log('onSelect');
-    // console.log(format(value[0]), format(value[1]));
-    selectedArr.push(format(value[0]), format(value[1]));
-    diffDate = moment.duration(value[1].diff(value[0]));
-
-
-    this.setState({selectedArr: selectedArr, diffDate: diffDate/86400000});
-
+    super(props)
+    const { startDate, endDate, onStandaloneSelect } = this.props
   }
 
   render() {
@@ -143,27 +44,22 @@ export class ResultCalendar extends React.Component {
             defaultValue={now}
             dateInputPlaceholder={['start', 'end']}
             locale={zhCN}
-            showOk={true}
             showClear
             format={formatStr}
-            onChange={onStandaloneChange}
-            onSelect={this.onStandaloneSelect}
-            disabledDate={disabledDate}
+            onSelect={this.props.onStandaloneSelect}
           />
         </div>
         <div style={divStyle}>
           <h4>
             請依您的出勤紀錄選擇對應的範圍日期
-            （理論上依八週需選兩個月外，其餘皆一個月）
+            （理論上除八週需選兩個月外，其餘皆一個月）
           </h4>
           <h5>您選的日期：</h5>
           <div>
-            <input type='textbox' value={this.state.selectedArr[0]} onChange={this.onStandaloneSelect}/>
-            <input type='textbox' value={this.state.selectedArr[1]} onChange={this.onStandaloneSelect}/>
+            <input type='textbox' value={this.props.startDate} onChange={this.props.onStandaloneSelect}/>
+            <span> ~ </span>
+            <input type='textbox' value={this.props.endDate} onChange={this.props.onStandaloneSelect}/>
           </div>
-          <span>差</span>
-          <input type='textbox' value={ this.state.diffDate } onChange={this.onStandaloneSelect}/>
-          <span>日</span>
         </div>
       </div>
     );
