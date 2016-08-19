2a85d6876e9d696ba385c126a45f867296843f7e
azure820529 <azure820529@gmail.com>
2016-08-19 16:00:57 +0800

    refactor dateinfo component into class syntax and make onclick dateInfoBox alert

--- components/DateInfoBox.js
+++ components/DateInfoBox.js
@@ -1,34 +1,48 @@
 import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
-const boxColor = (type) => {
-  let styleObj = {
-    'backgroundColor': 'white'
+
+class DateInfoBox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.boxColor = this.boxColor.bind(this)
   }
-  if(type == 'RB') {
-    return Object.assign({}, styleObj, {
-      'backgroundColor': 'red'
-    })
-  } else if (type == 'VB') {
-    return Object.assign({}, styleObj, {
-      'backgroundColor': 'blue'
-    })
-  } else if (type == 'SB') {
-    return Object.assign({}, styleObj, {
-      'backgroundColor': 'yellow'
-    })
-  } else {
-    return styleObj
+
+  boxColor(type) {
+    let styleObj = {
+      margin: '2px',
+      padding: '2px',
+      backgroundColor: 'white',
+      width: '12%',
+      textAlign: 'center',
+      fontSize: '14px'
+    }
+    if(type == 'RB') {
+      return Object.assign({}, styleObj, {
+        'backgroundColor': 'red'
+      })
+    } else if (type == 'VB') {
+      return Object.assign({}, styleObj, {
+        'backgroundColor': 'blue'
+      })
+    } else if (type == 'SB') {
+      return Object.assign({}, styleObj, {
+        'backgroundColor': 'yellow'
+      })
+    } else {
+      return styleObj
+    }
+  }
+
+  render() {
+    return (
+      <div className="col-xs-1" onClick={this.props.onClick} style={this.boxColor(this.props.data.workdayType)}>
+          <h4>{this.props.data.date.slice(5, 7) + '/' + this.props.data.date.slice(8, 10)}</h4>
+          <p>{this.props.data.workingHour}</p>
+      </div>
+    )
   }
 }
 
-const DateInfoBox = (diObj) => (
-  <div className="col-xs-1" style={boxColor(diObj.diObj.workdayType)}>
-    <h5>{diObj.diObj.date}</h5>
-    <p>
-      {diObj.diObj.workingHour}
-    </p>
-  </div>
-)
 
 export default DateInfoBox
--- components/DateInfoList.js
+++ components/DateInfoList.js
@@ -3,17 +3,32 @@ import ReactDOM from 'react-dom';
 import DateInfoBox from './DateInfoBox'
 
 
-const DateInfoList = ({ workDateInfo, diBoxOnclick }) => (
-  <div>
-    <h3>填入對應工時及該日型態</h3>
-    {workDateInfo.map((di, id) =>
-      <DateInfoBox
-        key={id}
-        diObj={di}
-      />
-    )}
-  </div>
-)
+class DateInfoList extends React.Component {
+  constructor(props) {
+    super(props)
+    const { workDateInfo } = this.props
+  }
+
+  onLoadDateInfo(e) {
+    console.log(e)
+  }
+
+  render() {
+    this.onLoadDateInfo = this.onLoadDateInfo.bind(this)
+    return (
+      <div className="col-xs-8" style={{paddingLeft: '0%'}}>
+        <h3>填入對應工時及該日型態</h3>
+        {this.props.workDateInfo.map((di, id) =>
+          <DateInfoBox
+            key={id}
+            data={di}
+            onClick={() => this.onLoadDateInfo(id)}
+          />
+        )}
+      </div>
+    )
+  }
+}
 
 DateInfoList.PropTypes = {
   workDateInfo: PropTypes.arrayOf(PropTypes.shape({
