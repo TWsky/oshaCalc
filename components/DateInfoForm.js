61104f4568b50a5f3d0d17f9825784cb5eb7c8bb
azure820529 <azure820529@gmail.com>
2016-08-23 23:51:49 +0800

    dateInfoForm Hour input fixed for can dispatch

--- components/DateInfoForm.js
+++ components/DateInfoForm.js
@@ -5,16 +5,12 @@ import { Button } from 'react-bootstrap'
 class DateInfoForm extends React.Component {
   constructor(props) {
     super(props)
-    const { data, curID, dayTypeDispatch } = this.props
-
     this.handleInput = this.handleInput.bind(this)
     this.updateDIObj = this.updateDIObj.bind(this)
   }
-
   handleInput(event) {
     this.updateDIObj('time', event.target.value)
   }
-
   updateDIObj(type, val) {
     switch (type) {
       case 'time':
@@ -23,21 +19,24 @@ class DateInfoForm extends React.Component {
           workingHour: val,
           workdayType: this.props.data[this.props.curID].workdayType
         })
+        break
       case 'DType':
         this.props.dayTypeDispatch(this.props.curID, {
           date: this.props.data[this.props.curID].date,
           workingHour: this.props.data[this.props.curID].workingHour,
           workdayType: val
         })
+        break
+      default:
+        console.log("No Change")
     }
   }
   render() {
     const { data, curID, dayTypeDispatch } = this.props
-
     return (
       <div style={{'textAlign': 'center'}}>
         <h2>
-          {this.props.data[this.props.curID].date.slice(5, 7) + '/' + this.props.data[this.props.curID].date.slice(8, 10)}
+          {data[curID].date.slice(5, 7) + '/' + data[curID].date.slice(8, 10)}
         </h2>
         <br/>
         <div>
@@ -45,15 +44,16 @@ class DateInfoForm extends React.Component {
           <input
             type='text'
             style={{width: '25%' ,margin: '5%' }}
-            value={this.props.data[this.props.curID].workingHour}
-            onChange={this.handleInput} />
+            value={data[curID].workingHour}
+            onChange={this.handleInput}
+            />
           <h4 style={{display: 'inline'}}>小時</h4>
         </div>
         <div>
-          <Button name='typeGroupBtn' style={{ width: '40%', margin: '5%'}} bsSize='lg' active={this.props.data[this.props.curID].workdayType == 'WD'} onClick={() => this.updateDIObj('DType', 'WD')}>上班日</Button>
-          <Button name='typeGroupBtn' style={{ width: '40%', margin: '5%'}} bsSize='lg' active={this.props.data[this.props.curID].workdayType == 'RB'} onClick={() => this.updateDIObj('DType', 'RB')}>例假</Button>
-          <Button name='typeGroupBtn' style={{ width: '40%', margin: '5%'}} bsSize='lg' active={this.props.data[this.props.curID].workdayType == 'VB'} onClick={() => this.updateDIObj('DType', 'VB')}>國定休假</Button>
-          <Button name='typeGroupBtn' style={{ width: '40%', margin: '5%'}} bsSize='lg' active={this.props.data[this.props.curID].workdayType == 'SB'} onClick={() => this.updateDIObj('DType', 'SB')}>特休</Button>
+          <Button name='typeGroupBtn' style={{ width: '40%', margin: '5%'}} bsSize='lg' active={data[curID].workdayType == 'WD'} onClick={() => this.updateDIObj('DType', 'WD')}>上班日</Button>
+          <Button name='typeGroupBtn' style={{ width: '40%', margin: '5%'}} bsSize='lg' active={data[curID].workdayType == 'RB'} onClick={() => this.updateDIObj('DType', 'RB')}>例假</Button>
+          <Button name='typeGroupBtn' style={{ width: '40%', margin: '5%'}} bsSize='lg' active={data[curID].workdayType == 'VB'} onClick={() => this.updateDIObj('DType', 'VB')}>國定休假</Button>
+          <Button name='typeGroupBtn' style={{ width: '40%', margin: '5%'}} bsSize='lg' active={data[curID].workdayType == 'SB'} onClick={() => this.updateDIObj('DType', 'SB')}>特休</Button>
         </div>
       </div>
     )
