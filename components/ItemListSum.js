90c9b68d354a17e3f64a35b356805d8487d86252
azure820529 <azure820529@gmail.com>
2016-09-15 18:18:42 +0800

    fix some wageList css

--- components/ItemListSum.js
+++ components/ItemListSum.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom'
 import { Button, Glyphicon } from 'react-bootstrap'
 
 const ItemListSum = ({title, itemList, sum, listChange, listPush, listPop}) => (
-  <div className="col-xs-offset-1 col-xs-10" style={{marginBottom: '6%'}}>
-    <h3>{title}</h3>
-    <div>
-      <table className="col-xs-3" style={{fontSize: '20px', textAlign: 'center'}}>
+  <div className={title == "該時段薪資" ? "col-xs-offset-1 col-xs-5" : "col-xs-5"} style={{height: '500px'}}>
+    <h3 style={{textAlign: 'center'}}>{title}</h3>
+    <div style={{margin: '0 auto', width: '360px', height: '350px'}}>
+      <table style={{fontSize: '20px', textAlign: 'center'}}>
         <thead>
           <tr>
             <th></th>
@@ -49,7 +49,7 @@ const ItemListSum = ({title, itemList, sum, listChange, listPush, listPop}) => (
         </tbody>
       </table>
     </div>
-    <div className="col-xs-offset-3 col-xs-4" style={{marginTop: '5%'}}>
+    <div style={{ margin: '40px auto 0', width: '270px' }}>
       <input type="text" value={sum} /><span style={{ fontSize: '20px', margin: '5px' }}>NTD</span>
     </div>
   </div>
